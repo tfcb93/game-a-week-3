ebc20fe4b26dc3fbdb154e9a2ee2d3a8d47d8137
Thiago Borges <thiago@tfcb93.com>
2024-11-12 15:10:55 -0300

    Changes blackjack code a bit to make it better to understand

--- Scripts/blackjack.gd
+++ Scripts/blackjack.gd
@@ -2,6 +2,7 @@ extends CanvasLayer;
 
 const awards: Array[String] = ["🍞", "🌮", "🥛", "🍯", "🍹", "🍶", "🍨"];
 const suits: Array[String] = ["spades", "hearts", "diamonds", "clubs"];
+const special_cards: Dictionary = {1: ["Ace", 11], 11: ["Jack", 10], 12: ["Queen", 10], 13: ["King", 10]};
 const bj_price: int = 20;
 
 var pile: Array = [];
@@ -9,21 +10,9 @@ var pile: Array = [];
 var table_keep_asking: bool = true;
 var is_game_started: bool = false;
 
-@onready var interface: VBoxContainer = $interface;
-@onready var result: VBoxContainer = $result;
-
-
-@onready var ask: Button = $interface/buttons/ask;
-@onready var stay: Button = $interface/buttons/stay;
-
-@onready var final_result: Label = $result/result;
-@onready var match_status: Label = $result/match_status;
-@onready var restart: Button = $result/restart;
-
-
 func _ready() -> void:
-	interface.visible = true;
-	result.visible = false;
+	%interface.visible = true;
+	%result.visible = false;
 	%player_hand.set_players_name("Player's hand");
 	%table_hand.set_players_name("Table's hand");
 	Events.connect("shuffle_blackjack", _on_shuffle_blackjack);
@@ -36,48 +25,28 @@ func generate_pile() -> void:
 	var cards = range(1, 13);
 	for suit in suits:
 		for card in cards:
-			var points: int;
-			var card_name: String;
-			match(card):
-				1:
-					points = 11;
-					card_name = "Ace"
-				11:
-					points = 10;
-					card_name = "Jack";
-				12:
-					points = 10;
-					card_name = "Queen";
-				13:
-					points = 10;
-					card_name = "King";
-				_:
-					points = card;
-					card_name = str(card);
-			pile.push_back([card_name, suit, points]);
+			var selected_card: Array = special_cards.get(card,[str(card), card]);
+			pile.push_back([selected_card[0], suit, selected_card[1]]);
 	randomize();
 	pile.shuffle();
 
 func reset_game() -> void:
-	interface.visible = true;
-	result.visible = false;
+	%interface.visible = true;
+	%result.visible = false;
 	if (Player.money < bj_price):
-		ask.disabled = true;
-		stay.disabled = true;
+		%ask.disabled = true;
+		%stay.disabled = true;
 		Events.emit_signal("send_text_to_dialog", "You don't have money to play this game");
 	else:
-		ask.disabled = false;
-		stay.disabled = false;
+		%ask.disabled = false;
+		%stay.disabled = false;
 		generate_pile();
 		
-	reset_hands();
+	%player_hand.reset_player();
+	%table_hand.reset_player();
 	table_keep_asking = true;
 	is_game_started = false;
 	
-func reset_hands() -> void:
-	%player_hand.reset_player();
-	%table_hand.reset_player();
-
 func _on_restart_pressed() -> void:
 	reset_game();
 
@@ -91,19 +60,18 @@ func _on_ask_pressed() -> void:
 	%player_hand.give_card(card);
 	
 	if (%player_hand.score >= 21):
-		ask.disabled = true;
-		stay.disabled = true;
+		%ask.disabled = true;
+		%stay.disabled = true;
 		if (not table_keep_asking):
 			finish_game();
 		else:
 			keep_playing();
 	else:
 		ask_table();
-	
 
 func _on_stay_pressed() -> void:
-	ask.disabled = true;
-	stay.disabled = true;
+	%ask.disabled = true;
+	%stay.disabled = true;
 	if (table_keep_asking):
 		keep_playing();
 	else:
@@ -133,7 +101,7 @@ func should_table_keep_asking() -> void:
 	elif (%table_hand.score > 19 and randi() % 2 + 1 == 1):
 		table_keep_asking = false;
 	
-	if (ask.disabled and not table_keep_asking):
+	if (%ask.disabled and not table_keep_asking):
 		finish_game();
 
 func keep_playing() -> void:
@@ -141,31 +109,31 @@ func keep_playing() -> void:
 		ask_table();
 
 func finish_game() -> void:
-	interface.visible = false;
-	result.visible = true;
+	%interface.visible = false;
+	%result.visible = true;
 	
-	match_status.text = "You score was: %d and the table was: %d" % [%player_hand.score, %table_hand.score];
+	%match_status.text = "You score was: %d and the table was: %d" % [%player_hand.score, %table_hand.score];
 	if (%player_hand.score <= 21 and %table_hand.score <= 21):
 		if (%player_hand.score < %table_hand.score):
-			final_result.text = "You lose";
+			%final_result.text = "You lose";
 			Player._add_to_inventory(awards[0]);
 			Events.emit_signal("send_text_to_dialog", "You got: " + "[emote]" + awards[0] + "[/emote]");
 		else:
-			final_result.text = "You win";
+			%final_result.text = "You win";
 			give_award();
 	elif(%table_hand.score > 21 and %player_hand.score <= 21):
-		final_result.text = "You win";
+		%final_result.text = "You win";
 		give_award();
 	else:
-		final_result.text = "You lose";
+		%final_result.text = "You lose";
 		Player._add_to_inventory(awards[0]);
 		Events.emit_signal("send_text_to_dialog", "You got: " + "[emote]" + awards[0] + "[/emote]");
 	
 func _on_check_player_money() -> void:
 	if (Player.money >= bj_price):
-		restart.disabled = false;
+		%restart.disabled = false;
 	else:
-		restart.disabled = true;
+		%restart.disabled = true;
 
 func give_award() -> void:
 	var award: String;
